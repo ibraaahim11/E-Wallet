d5cded437c2652cc822c2e525ab247406f011a35
ibraaahim11 <ibrahimhesham21@gmail.com>
2025-08-10 18:24:40 +0300

    inital set up with h2 db

---
+++ src/main/java/com/vois/e_wallet/EWalletApplication.java
@@ -0,0 +1,13 @@
+package com.vois.e_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWalletApplication.class, args);
+	}
+
+}
